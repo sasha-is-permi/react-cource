023ccf38563fdfcf2762e42ece4d995b505d957d
Alexander <sasha-is-permi@mail.ru>
2025-11-21 18:24:41 +0500

    dz-2

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,42 @@
-import { restaurants } from './constants';
+import { restaurants } from './constant';
+import './App.css';
 
 function App() {
   return (
-    <div>
-      <h1>Рестораны</h1>
-      <ul className='restaurants__list'>
+    <div className="page">
+      <h1 className="page__title">Рестораны</h1>
+      <div className="restaurant-list">
         {restaurants.map((restaurant) => (
-          <li className='restaurants__list-block' key={restaurant.id}>
-            <h2>{restaurant.name}</h2>
-            <div className='menu__list'>
-              <h3>Меню</h3>
-              <ul className='menu__list-block'>
+          <div className="restaurant-card" key={restaurant.id}>
+            <h2 className="restaurant-card__title">{restaurant.name}</h2>
+            
+            <div className="dish-menu">
+              <h3 className="dish-menu__title">Меню</h3>
+              <ul className="dish-menu__list">
                 {restaurant.menu.map((item) => (
-                  <li key={item.id}>
-                    {item.name} - {item.price}$
+                  <li className="dish-menu__item" key={item.id}>
+                    <span className="dish-menu__name">{item.name}</span>
+                    <span className="dish-menu__price">{item.price}$</span>
                   </li>
                 ))}
               </ul>
             </div>
-            <div className='reviews__list'>
-              <h3>Отзывы</h3>
-              <ul className='reviews__list-block'>
+            
+            <div className="feedback">
+              <h3 className="feedback__title">Отзывы</h3>
+              <ul className="feedback__list">
                 {restaurant.reviews.map((item) => (
-                  <li key={item.id}>
-                    <div className='review__block'>
-                        <p className='review__block-name'>{item.user}:</p>
-                        <p className='review__block-text'>{item.text}</p>
-                        <p className='review__block-rating'>{item.rating}</p>
-                    </div>
+                  <li className="feedback__item" key={item.id}>
+                    <p className="feedback__author">{item.user}</p>
+                    <p className="feedback__text">{item.text}</p>
+                    <p className="feedback__rating">Рейтинг: {item.rating}</p>
                   </li>
                 ))}
               </ul>
             </div>
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 }
